9390ee15761ed014d70a35e2605642f0c21296e8
Cookici <632832232@qq.com>
2023-06-09 13:52:55 +0800

    优化

--- src/com/lls/sms/utils/MD5Util.java
+++ src/com/lls/sms/utils/MD5Util.java
@@ -1,7 +1,6 @@
 package com.lls.sms.utils;
 
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -25,7 +24,7 @@ public class MD5Util {
             secretBytes = MessageDigest.getInstance("md5").digest(
                     plainText.getBytes());
         } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("没有md5这个算法！");
+            throw new RuntimeException(e);
         }
         StringBuilder md5code = new StringBuilder(new BigInteger(1, secretBytes).toString(16));// 16进制数字
         // 如果生成数字未满32位，需要前面补0
